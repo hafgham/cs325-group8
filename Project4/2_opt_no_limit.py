6f15cb6c86e27667a7befccc507db020d6842086
smidtj <smidtj@onid.oregonstate.edu>
2015-08-12 16:19:00 +0100

    added two iterative implimentations of TSP using 2-opt. In file 2_opt_5min.py, the iterations are limited to 5min and output is classified as .tour_5min. In file 2_opt_no_limit.py there are no time limits and output is classified as .tour_no_limit.  Running time and shortest path numbers can be found in 2_opt_5min_results.txt

--- Project4/2_opt_no_limit.py
+++ Project4/2_opt_no_limit.py
@@ -7,8 +7,7 @@ from sys import argv;
 #Function returns distance between two cities, rounded to float, cast to int
 
 file_name = argv[1]
-points = []
-size = 550
+
 def get_distance(c1, c2):
 	return int(round(sqrt((c1[1]-c2[1])*(c1[1]-c2[1]) + (c1[2]-c2[2])*(c1[2]-c2[2]))))
 
@@ -26,15 +25,6 @@ with open(file_name, "r") as text_file:
 		cities[i].append(int(line.strip().split()[2]))
 		i = i + 1
 
-#2D list to hold disatances between cities, accessed by city id:
-#e.g. distances[0][1] is the distance between city the first city in list and the second
-distances = [[0 for i in range(len(cities))]for j in range(len(cities))]
-
-#get distances between all cities
-for i in range(len(cities)):
-	for j in range(len(cities)):
-		distances[i][j] = get_distance(cities[i], cities[j]);
-
 #get distances between all cities
 def distance(cities):
 	if len(cities) == 0:
@@ -49,20 +39,33 @@ def two_opt(cities):
   for i in range(len(cities) - 1):
     for j in range(i + 2, len(cities) - 1):
       if get_distance(cities[i], cities[i+1]) + get_distance(cities[j], cities[j+1]) > get_distance(cities[i], cities[j]) + get_distance(cities[i+1], cities[j+1]):          cities[i+1:j+1] = reversed(cities[i+1:j+1])
-  return cities
 
 
-#As two_opt loops through the circuit, breakig two conections at a time and improving them, and out implementation of two_opt makes a loop of all nodes, each call will most likely be an incremental improvement.  Therefore, if we have a time period of 5min, it would be best to make as many calls of two-opt as possible in that time period.
+
+#As two_opt loops through the circuit, breakig two conections at a time and improving them, and each call of two_opt makes a loop of all nodes, each call will most likely be an incremental improvement.  Therefore, if we have a time period of 5min, it would be best to make as many calls of two-opt as possible in that time period that still improve the overall circuit.  Essentially LP.
 single_run_time = datetime.timedelta(microseconds=0);
 total_diff = datetime.timedelta(microseconds=0);
+old_length = distance(cities) + 1
+new_length = distance(cities)
 #check to see if an additional run will cause run time to go past ~5min
-while ((total_diff.seconds + single_run_time.seconds) < 300):
-	t1 = datetime.datetime.now()	
+while (old_length > new_length):
+	#first get time
+	t1 = datetime.datetime.now()
+
+	#core algorithm that minimizes 2-opt distance in min time
+	old_length = new_length	
 	two_opt(cities)	
+	new_length = distance(cities)
+
+
+	#second get time
 	t2 = datetime.datetime.now()
 	single_run_time = t2 - t1
 	total_diff = total_diff + single_run_time
+	#clear screen
+	sys.stderr.write("\x1b[2J\x1b[H")
 	print 'Execution time: ', total_diff.seconds, 's', int(total_diff.microseconds/1000), 'ms'
+	
 
 print 'Final Execution time: ', total_diff.seconds, 's', int(total_diff.microseconds/1000), 'ms'
 
@@ -71,7 +74,7 @@ cities_order = tuple(x[0] for x in cities)
 
 #build the array as a string for printing to file
 
-output = file_name + '.tour'
+output = file_name + '.tour_no_limit'
 file = open(output, "w")
 
 file.write("%i\n" % distance(cities))
